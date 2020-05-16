a5b17a1dd14b0be90c6ad1e7df898c7c808686f5
Rey David Cuevas <cuevas.vela@gmail.com>
2020-05-16 17:04:43 -0500

    added helper method to chec if user is logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,4 +17,11 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please Log In'
+      redirect_to login_url
+    end
+  end
 end
