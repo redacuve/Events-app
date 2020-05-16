73c43c97afa138c675529636a9d30c2294737c5e
Rey David Cuevas <cuevas.vela@gmail.com>
2020-05-16 17:04:25 -0500

    added method to check if a user is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,11 +32,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = 'Please Log In'
-      redirect_to login_url
-    end
-  end
 end
